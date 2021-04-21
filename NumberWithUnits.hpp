b6529296e4f39040c622f0d2c7a4f96f23e939c8
danielberco <59802615+danielberco@users.noreply.github.com>
2021-04-21 18:05:39 +0300

    Part A

--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -3,147 +3,157 @@
  * Author: Daniel Berkovich
  * http://github.com/danielberco
  */
-
-#include <map>
-#include<iostream>
+#include "NumberWithUnits.hpp"
+#include <iostream>
+#include <stdexcept>
+#include <exception>
 #include <string>
+#include <map>
+#include <fstream>
+
+
 using namespace std;
+
+
 namespace ariel {
-    class NumberWithUnits{
+    static map <string,map<string,double>> tab;
 
-    private:
-        double amountUnit;
-        string unit;
 
-    public:
-        NumberWithUnits(double amountUnit, string unit){
-            this->amountUnit = amountUnit;
-            this->unit = unit;
+
+    double convertUnit (const string u1, const string u2, const double uNum) {
+        if(u1 == u2) {
+            return uNum;
+        }
+        try {
+            return tab.at(u1).at(u2) * uNum;
+        }
+        catch (const std::exception& exception) {
+            throw invalid_argument{"Error:  Cannot convert from "+ u1 +" to "+ u2};
+        }
+    }
+
+    double checkUnit (const string u1, const string u2, double n) {
+        if (u1 == u2) {
+            return n;
         }
 
+        try {
+            return n * tab[u1][u2];
+        }
+        catch(const exception& exe) {
+            throw invalid_argument("Cannot convet units");
 
-           
-            ~NumberWithUnits(){}
-            static void read_units(ifstream& units_file);
-
-            //ostream & istream
-             friend ostream& operator<<(ostream& ostream , const NumberWithUnits& n1);
-             friend istream& operator>>(istream& istream , NumberWithUnits& n1);
-
-             /**
-              * Operator +
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend NumberWithUnits operator+(const NumberWithUnits& n1 , const NumberWithUnits& n2);
-             friend NumberWithUnits operator+(const NumberWithUnits& n1 , const double n3);
-
-             /**
-              * Operator +=
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-
-             friend NumberWithUnits operator+=( NumberWithUnits& n1 ,const NumberWithUnits& n2);
-            
-
-             /**
-              * Operator -
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend NumberWithUnits operator-( const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend NumberWithUnits operator-( const NumberWithUnits& n1);
-
-             
-             
-
-              /**
-              * Operator -=
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend NumberWithUnits operator-=( NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend NumberWithUnits operator-=( NumberWithUnits& n1 ,const double n3);
-            
-
-              /**
-              * Operator > & Operator < 
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend bool operator>( const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend bool operator>( NumberWithUnits& n1 ,const double n3);
-             friend bool operator>(const double n3, const NumberWithUnits& n1);
-             friend bool operator<( const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend bool operator<( const NumberWithUnits& n1 ,const double n3);
-             friend bool operator<(const double n3, const NumberWithUnits& n1);
-
-             /**
-              * Operator >= & Operator <= 
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend bool operator>=(const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend bool operator>=(const NumberWithUnits& n1 ,const double n3);
-             friend bool operator>=(const double n3, const NumberWithUnits& n1);
-             friend bool operator<=( const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend bool operator<=( NumberWithUnits& n1 ,const double n3);
-             friend bool operator<=(const double n3, const NumberWithUnits& n1);
-
-             /**
-              * Operator == < 
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend bool operator==( const NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend bool operator==( NumberWithUnits& n1 ,const double n3);
-             friend bool operator==(const double n3, const NumberWithUnits& n1);
+        }
+    }
 
 
+    void addUnits(const string u1, const string u2) {
+        for (const auto &at : tab[u1]) {
+            if (at.first != u2) {
+                double val = at.second*tab[u2][u1];
+                tab[at.first][u2] = 1/val;
+                tab[u2][at.first] = val;
+            }
+        }
+    }
 
-             /**
-              * Operator ++
-              * 
-              * 
-              **/
-             friend NumberWithUnits operator++(NumberWithUnits& n1);
-             friend NumberWithUnits operator++(NumberWithUnits&n1,int);
+    void printTable(){
+        for(const auto &i : tab) {
+            for(const auto&j : tab[i.first]) {
+                        std::cout <<"tab["<< i.first << "][" << j.first << "] => " << j.second << endl;
+             }
+        }
+    }
 
-             /**
-              * Operator --
-              * 
-              * 
-              **/
-             friend NumberWithUnits operator--(NumberWithUnits& n1);
-             friend NumberWithUnits operator--(NumberWithUnits&n1,int);
+     void NumberWithUnits::read_units(ifstream& units_file) {
+        string u1,u2,op;
+        double x,y;
+        while(units_file >> x >> u1 >> op >> y >> u2){
+            tab[u1][u2] = y;
+            tab[u2][u1] = 1/y;
+            addUnits(u1,u2);
+            addUnits(u2,u1);
+        }
+        printTable();
+    }
+
+    /**
+     * Overloading operator + 
+     * 
+     * 
+     * */
+
+    NumberWithUnits operator+(const NumberWithUnits& n1,const NumberWithUnits& n2) {
+       double val = convertUnit(n2.unit,n1.unit,n2.amountUnit);
+       return NumberWithUnits(n1.amountUnit+val,n1.unit); 
+    }
+
+    NumberWithUnits operator+(const NumberWithUnits& n1,const double n3) {
+       return NumberWithUnits(n1.amountUnit+n3,n1.unit); 
+    }
+
+    /**
+     * Overloading operator -
+     * 
+     * 
+     * */
+
+    NumberWithUnits operator-(const NumberWithUnits& n1,const NumberWithUnits& n2) {
+        double x = convertUnit(n2.unit,n1.unit,n2.amountUnit);
+        return NumberWithUnits(n1.amountUnit-x,n1.unit);
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits& n1) {
+        return NumberWithUnits(-n1.amountUnit,n1.unit);
+    }
+
+    NumberWithUnits operator+=(NumberWithUnits& n1,const NumberWithUnits& n2) {
+        double x = convertUnit(n2.unit,n1.unit,n2.amountUnit);
+        n1.amountUnit += x;
+        return n1;
+    }
+
+    NumberWithUnits operator-=(NumberWithUnits& n1,const NumberWithUnits& n2) {
+        double x = convertUnit(n2.unit,n1.unit,n2.amountUnit);
+        n1.amountUnit -= x;
+        return NumberWithUnits(n1.amountUnit, n1.unit);   
+    }
+
+    bool operator>(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit>checkUnit(n2.unit,n1.unit,n2.amountUnit);
+    }
+
+    bool operator<(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit<checkUnit(n2.unit,n1.unit,n2.amountUnit);
+    }
+
+    bool operator>=(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit>=checkUnit(n2.unit,n1.unit,n2.amountUnit);
+    }
+
+    bool operator<=(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit<=checkUnit(n2.unit,n1.unit,n2.amountUnit);
+    }
+
+    bool operator==(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit>=checkUnit(n2.unit,n1.unit,n2.amountUnit);
+  }
+
+  bool operator!=(const NumberWithUnits& n1, const NumberWithUnits& n2) {
+        return n1.amountUnit != checkUnit(n2.unit,n1.unit,n2.amountUnit);
+  }
+
+  ostream& operator<<(ostream& out, const NumberWithUnits& unit){
+      out << unit.amountUnit << "["<< unit.unit << "]";
+      return out;
+  }
+
+  istream& operator>>(istream& ini, NumberWithUnits& s){
+     string str;
+      ini >> s.amountUnit >> str >> s.unit;
+      return ini;
+  }
 
 
-             /**
-              * Operator *
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend NumberWithUnits operator*( NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend NumberWithUnits operator*( NumberWithUnits& n1 ,const double n3);
-             friend NumberWithUnits operator*(const double n3, const NumberWithUnits& n1);
-
-             /**
-              * Operator /
-              * n1 - x
-              * n2 - y
-              * n3 - number (double)
-              **/
-             friend NumberWithUnits operator/( NumberWithUnits& n1 ,const NumberWithUnits& n2);
-             friend NumberWithUnits operator/( NumberWithUnits& n1 ,const double n3);
-             friend NumberWithUnits operator/(const double n3, const NumberWithUnits& n1);      
-     };
 }
+
